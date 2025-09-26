80dff32ff696941d8ee3d5fd2a7c5293091d389e
fadhil <f2daengnaba@gmail.com>
2025-09-26 15:59:06 +0700

    original fragment

--- drill/drill.cpp
+++ drill/drill.cpp
@@ -3,7 +3,10 @@
 int main()
 {
 	try {
-		// your code
+		string res = 7;
+		vector<int> v(10);
+		v[5] = res;
+		cout << "Success!\n";
 	}
 	catch (exception& e) {
 		cerr << "error: " << e.what() << '\n';
