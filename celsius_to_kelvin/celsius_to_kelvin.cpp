5c1ebd933efba02a394351391748887c1eb19e82
fadhil <f2daengnaba@gmail.com>
2025-09-26 18:58:28 +0700

    listt what wrong with ctok

---
+++ celsius_to_kelvin/celsius_to_kelvin.cpp
@@ -0,0 +1,15 @@
+#include <std_lib_facilities.h>
+
+double ctok(double c)
+{
+	int k = c + 273.15; // narrowing
+	return int; // return expect double
+}
+
+int main()
+{
+	double c = 0;
+	cin >> d;
+	double k = ctok("c"); // a char???
+	Cout << k << '\n'; // it should be cout, 
+}
