6f942abff297dfade0ef80ea9dbbc42fbcc50148
fadhil <f2daengnaba@gmail.com>
2025-09-26 15:47:06 +0700

    add drill scaffolding

---
+++ drill/drill.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	try {
+		// your code
+	}
+	catch (exception& e) {
+		cerr << "error: " << e.what() << '\n';
+		keep_window_open();
+		return 1;
+	}
+	catch (...) {
+		cerr << "Oops: unknown exception!\n";
+		keep_window_open();
+		return 2;
+	}
+}
