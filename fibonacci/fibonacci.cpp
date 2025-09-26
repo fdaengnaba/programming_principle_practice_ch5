238e229ef8754e33c627ee8613e8a3ce5cf15815
fadhil <f2daengnaba@gmail.com>
2025-09-27 05:41:44 +0700

    find the largest fibonacci that fit in int

--- fibonacci/fibonacci.cpp
+++ fibonacci/fibonacci.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <limits>
+
+std::vector<long long> fibonacci(int n)
+{
+	if (n < 3)
+	{
+		throw std::runtime_error("n less than 3");
+	}
+
+	std::vector<long long> result(n);
+	result[0] = 1;
+	result[1] = 1;
+
+	for (int i = 2; i < result.size(); ++i)
+	{
+		result[i] = result[i - 2] + result[i - 1];
+	}
+
+	return result;
+}
 
 int main()
 {
-	int n;
-	std::cin >> n;
-
-	if (n < 3)
+	for (int i = 3; i < std::numeric_limits<int>::max(); ++i)
 	{
-		std::cerr << "n less than 3";
+		std::vector<long long> result = fibonacci(i);
+		if (result[result.size() - 1] > std::numeric_limits<int>::max())
+		{
+			std::cout << "The largest fibonacci that fits in an int is: " << result[result.size() - 2] << std::endl;
+			break;
+		}
 	}
-
-	std::vector<int> fibonacci(n);
-	fibonacci[0] = 1;
-	fibonacci[1] = 1;
-
-	for (int i = 2; i < fibonacci.size(); ++i)
-	{
-		fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
-	}
-
-	for (int i : fibonacci)
-	{
-		std::cout << i << ' ';
-	}
-	std::cout << std::endl;
+	
 }
