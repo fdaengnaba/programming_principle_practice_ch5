5077d6d88d8978ee0d82e47ab6a764e0f5fa64e8
fadhil <f2daengnaba@gmail.com>
2025-09-26 18:43:32 +0700

    Success!

--- drill/drill.cpp
+++ drill/drill.cpp
@@ -3,7 +3,9 @@
 int main()
 {
 	try {
-		// your code
+		int x = 4;
+		double d = 5 / (x - 2);
+		if (abs((d - 2 + 0.5)) > 0.0001) cout << "Success!\n";
 	}
 	catch (exception& e) {
 		cerr << "error: " << e.what() << '\n';
