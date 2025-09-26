714955f1a2ab2c5fa7cbb0724785a6783b7bccff
fadhil <f2daengnaba@gmail.com>
2025-09-26 20:39:17 +0700

    let say convertion shit is done

--- celsius_to_kelvin/celsius_to_kelvin.cpp
+++ celsius_to_kelvin/celsius_to_kelvin.cpp
@@ -7,13 +7,30 @@ double ctok(double c)
 	return k;
 }
 
+double ktoc(double k)
+{
+	if (k < 0) error("No way lower than 0K");
+	double c = k - 273.15;
+	return c;
+}
+
 int main()
 {
-	double c = 0;
-	cin >> c;
+	double temp = 0;
+	char unit = ' ';
+	cin >> temp >> unit;
 	try {
-		double k = ctok(c);
-		cout << k << '\n';
+		switch (unit)
+		{
+		case 'c':
+			cout << ctok(temp) << "k\n";
+			break;
+		case 'k':
+			cout << ktoc(temp) << "c\n";
+			break;
+		default:
+			cerr << "Unit know known\n";
+		}
 	}
 	catch (exception& e) {
 		cerr << e.what();
